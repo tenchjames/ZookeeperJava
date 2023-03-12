48ec89cb661d86887f8f5693c57aa0d2e651a4c3
James Tench <tenchjames@gatech.edu>
2023-03-12 14:24:07 -0400

    project completion

---
+++ Topics/Primitive and reference types/Creating a reference type/src/Main.java
@@ -0,0 +1,7 @@
+public class Main {
+    public static void main(String[] args) {
+        // put your code here
+        String learner = "Hello, learner!";
+        System.out.println(learner);
+    }
+}
